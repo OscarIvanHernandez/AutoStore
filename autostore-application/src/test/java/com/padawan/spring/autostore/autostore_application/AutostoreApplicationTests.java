bb45f4ad22de33b9e55ed4994fab940fdab5ca23
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-06-29 21:13:20 +0000

    Creacion de los archivos para el backend con SpringBoot

---
+++ autostore-application/src/test/java/com/padawan/spring/autostore/autostore_application/AutostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padawan.spring.autostore.autostore_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutostoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
