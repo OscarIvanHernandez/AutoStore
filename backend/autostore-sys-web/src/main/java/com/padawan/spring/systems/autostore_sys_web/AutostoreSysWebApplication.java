c0c07ac0c48e2d496dc379d023627c524e783c65
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-01 20:15:42 +0000

    Restructuración de archivos; organizacion separada para backend y frontend; archivos para los contenedores

---
+++ backend/autostore-sys-web/src/main/java/com/padawan/spring/systems/autostore_sys_web/AutostoreSysWebApplication.java
@@ -0,0 +1,13 @@
+package com.padawan.spring.systems.autostore_sys_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutostoreSysWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutostoreSysWebApplication.class, args);
+	}
+
+}
---
+++ backend/autostore-sys-web/src/test/java/com/padawan/spring/systems/autostore_sys_web/AutostoreSysWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padawan.spring.systems.autostore_sys_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutostoreSysWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
