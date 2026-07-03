aa21c8e7a991be8165ce6d9a820d0e7f4b0dacb8
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-03 17:46:11 +0000

    Se creo una nueva entidad para el Movimiento en inventario

---
+++ backend/autostore-sys-web/src/main/java/com/autostore/sys/model/MovimientoInventario.java
@@ -0,0 +1,32 @@
+package com.autostore.sys.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "movimientos_inventario")
+@Data
+public class MovimientoInventario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id", nullable = false) // Asegura que cada movimiento esté asociado a un producto
+    private Producto producto;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoMovimiento tipoMovimiento;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(nullable = false)
+    private String motivo;
+
+    @Column(nullable = false)
+    private LocalDateTime fecha = LocalDateTime.now();
+}
