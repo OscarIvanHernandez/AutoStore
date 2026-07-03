3a42ac8a0f26374fd29fac4085a14955ccbdd689
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-03 18:14:14 +0000

    Agregar repositorio para la entidad MovimientoInventario

---
+++ backend/autostore-sys-web/src/main/java/com/autostore/sys/repository/MovimientoInventarioRepository.java
@@ -0,0 +1,8 @@
+package com.autostore.sys.repository;
+
+import com.autostore.sys.model.MovimientoInventario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovimientoInventarioRepository extends JpaRepository<MovimientoInventario, Long> {
+
+}
