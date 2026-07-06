7d3d1f80fed1dd3d1e4587a8b2870abe7d05fa4a
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-06 18:56:30 +0000

    IMplementación de: *AdminLayout *Navbar *Aside (bar) Asi como un interface para los modelos del back y un servicio para manejar los puntos de la API (EN LIMPIO TODO)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AdminLayout} from './shared/admin-layout/admin-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Espacio para rutas publicas
+
+  //
+
+  // Rutas privadas
+  { path: 'tablero',
+    component: AdminLayout,
+    children:[
+
+    ]
+  }
+];
