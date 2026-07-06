3d36dde5b6e1746e73a86a3c4d5784211be991f6
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-06 21:00:30 +0000

    Se agregaron las interface: *Producto *AjusteRequest

--- frontend/src/app/services/autostore.models.ts
+++ frontend/src/app/services/autostore.models.ts
@@ -1,1 +1,24 @@
-export interface Autostore {}
+export interface Producto {
+  id?: number;
+  nombre: string;
+  marca: string;
+  categoria: string;
+  proveedor?: string;
+  precioCompra: number;
+  precioVentaMostrador: number;
+  precioVentaTaller: number;
+  stockActual: number;
+  stockMinimo: number;
+  foto?: string;
+  activo?: boolean;
+  fechaCracion: string;
+  fechaActualizacion: string;
+
+}
+
+export interface AjusteRequest {
+  tipo: 'ENTRADA' | 'SALIDA';
+  cantida: number;
+  motivo: string;
+
+}
