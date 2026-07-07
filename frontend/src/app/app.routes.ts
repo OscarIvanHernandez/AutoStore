15abc9b73c80286807c953344ba59987c1042640
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-07 18:42:41 +0000

    Correción en los host permitidos en el docker;  Se modifico la estructura de las rutas;  Se agrego el template base para el AdminLayout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { AdminLayout} from './shared/admin-layout/admin-layout';
+import { Dashboard } from './features/admin/dashboard/dashboard';
+import { Products } from './features/admin/products/products';
 
 export const routes: Routes = [
   // Espacio para rutas publicas
@@ -7,10 +9,11 @@ export const routes: Routes = [
   //
 
   // Rutas privadas
-  { path: 'tablero',
+  { path: 'admin',
     component: AdminLayout,
     children:[
-
+      {path: 'tablero', component: Dashboard},
+      {path: 'productos', component: Products},
     ]
   }
 ];
