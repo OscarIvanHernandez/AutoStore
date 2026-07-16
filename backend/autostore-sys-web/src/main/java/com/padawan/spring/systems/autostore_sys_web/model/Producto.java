53f310fe253407aa5015a08348d0ade04f2f8c06
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-16 21:41:53 +0000

    Nuevas validaciones para el modelo de Producto en sus atributos

--- backend/autostore-sys-web/src/main/java/com/padawan/spring/systems/autostore_sys_web/model/Producto.java
+++ backend/autostore-sys-web/src/main/java/com/padawan/spring/systems/autostore_sys_web/model/Producto.java
@@ -1,6 +1,10 @@
 package com.padawan.spring.systems.autostore_sys_web.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -22,29 +26,38 @@ public class Producto {
     private Long id;
 
     @Column(nullable = false, length = 150)
+    @NotBlank(message = "El nombre del producto no puede estar vacío")
     private String nombre;
 
     @Column(nullable = false, length = 100)
+    @NotBlank(message = "La marca es obligatoria")
     private String marca;
 
     @Column(nullable = false, length = 100)
+    @NotBlank(message = "La categoría es obligatoria")
     private String categoria;
 
     private String proveedor;
 
     @Column(nullable = false, precision = 19, scale = 2)
+    @NotNull(message = "El precio de compra es obligatorio")
+    @Positive(message = "El precio de compra debe ser mayor a $0")
     private BigDecimal precioCompra;
 
     @Column(nullable = false,precision = 19, scale = 2)
+    @NotNull(message = "El precio de venta para mostrador es obligatorio")
+    @Positive(message = "Precio de venta bajo, debe ser mayor a 0")
     private BigDecimal precioVentaMostrador;
     
     @Column(precision = 19, scale = 2)
+    @Min(value = 0, message = "El precio de venta para taller no puede ser negativo")
     private BigDecimal precioVentaTaller;
 
     @Column(nullable = false)
     private Integer stockActual;
 
     @Column(nullable = false)
+    @Min(value = 0, message = "El stock mínimo no puede ser un número negativo")
     private Integer stockMinimo;
 
     private String foto;
