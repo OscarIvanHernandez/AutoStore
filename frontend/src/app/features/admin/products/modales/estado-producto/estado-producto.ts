775f2792a86e47ab80a1c3495ff0ac68cc54a4ce
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-22 19:33:03 +0000

    Correcion en la alineacion de icono don el texto de error en inputs; Nuevos componentes agregados para los distintos modales

---
+++ frontend/src/app/features/admin/products/modales/estado-producto/estado-producto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estado-producto',
+  imports: [],
+  templateUrl: './estado-producto.html',
+  styleUrl: './estado-producto.css',
+})
+export class EstadoProducto {}
---
+++ frontend/src/app/features/admin/products/modales/inventario-producto/inventario-producto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventario-producto',
+  imports: [],
+  templateUrl: './inventario-producto.html',
+  styleUrl: './inventario-producto.css',
+})
+export class InventarioProducto {}
