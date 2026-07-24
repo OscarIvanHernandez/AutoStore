7057bb173e95d37a97cd864888c0a3ee019b6658
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-24 20:25:27 +0000

    Lógica añadida para cambiar el estado del producto

--- frontend/src/app/features/admin/products/modales/estado-producto/estado-producto.ts
+++ frontend/src/app/features/admin/products/modales/estado-producto/estado-producto.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { ProductoInterface } from '../../../../../services/autostore.models';
 
 @Component({
   selector: 'app-estado-producto',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatIconModule],
   templateUrl: './estado-producto.html',
   styleUrl: './estado-producto.css',
 })
-export class EstadoProducto {}
+export class EstadoProducto implements OnChanges {
+  @Input() visible = false;
+  @Input() producto: ProductoInterface | null =null;
+
+  @Output() close = new EventEmitter<void>();
+  @Output() update = new EventEmitter<ProductoInterface>();
+
+  productoEstado: ProductoInterface | null = null;
+
+  ngOnChanges() {
+    if(this.producto) {
+      this.productoEstado = {...this.producto};
+    } else {
+      this.productoEstado = null;
+    }
+  }
+
+  guardarCambios() {
+    if (this.productoEstado) {
+      this.update.emit({...this.productoEstado});
+    }
+    this.cerrar();
+  }
+
+  cerrar() {
+    this.close.emit();
+  }
+}
