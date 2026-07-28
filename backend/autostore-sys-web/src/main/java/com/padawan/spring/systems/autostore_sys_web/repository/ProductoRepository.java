9792857476b1c53a2956250979848abab4c9a506
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-28 19:39:27 +0000

    Se agrego al repositorio metodos avanzados  con JPA para la realizacion de busquedas en los Productos

--- backend/autostore-sys-web/src/main/java/com/padawan/spring/systems/autostore_sys_web/repository/ProductoRepository.java
+++ backend/autostore-sys-web/src/main/java/com/padawan/spring/systems/autostore_sys_web/repository/ProductoRepository.java
@@ -1,6 +1,7 @@
 package com.padawan.spring.systems.autostore_sys_web.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,7 +10,7 @@ import com.padawan.spring.systems.autostore_sys_web.model.Producto;
 import java.util.List;
 
 // Repositorio para la entidad Producto, extendiendo JpaRepository para operaciones CRUD y consultas personalizadas
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
+public interface ProductoRepository extends JpaRepository<Producto, Long>, JpaSpecificationExecutor<Producto> {
 
     // Productos que no han sufrido eliminación lógica
     List<Producto> findByActivoTrue(); // Productos activos
