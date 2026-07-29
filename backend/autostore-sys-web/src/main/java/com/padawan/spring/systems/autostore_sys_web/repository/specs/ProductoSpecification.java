f849e657690d15cc33d501cba1d1921bf160161a
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-29 18:16:24 +0000

    Correción lógica: ahora la busqueda puede manejar la categoria y la busqueda por estado  cambio para que el usuario no maneje boleanos de forma directa en la busqueda

--- backend/autostore-sys-web/src/main/java/com/padawan/spring/systems/autostore_sys_web/repository/specs/ProductoSpecification.java
+++ backend/autostore-sys-web/src/main/java/com/padawan/spring/systems/autostore_sys_web/repository/specs/ProductoSpecification.java
@@ -10,23 +10,30 @@ import java.util.List;
 
 public class ProductoSpecification {
 
-    public static Specification<Producto> conFiltros(String q, String categoria, Boolean activo){
+    public static Specification<Producto> conFiltros(String q, String categoria, String estado){
         return (root, query, cb) -> {
             List<Predicate> predicates = new ArrayList<>();
-            // Búsqueda global por texto (Nombre o Marca)
+            // Búsqueda global por texto
             if (q != null && !q.trim().isEmpty()) {
                 String pattern = "%" + q.toLowerCase().trim() + "%";
                 Predicate nombreMatch = cb.like(cb.lower(root.get("nombre")), pattern);
                 Predicate marcaMatch = cb.like(cb.lower(root.get("marca")), pattern);
-                predicates.add(cb.or(nombreMatch, marcaMatch));
+                Predicate categoriaMatch = cb.like(cb.lower(root.get("categoria")), categoria);
+                predicates.add(cb.or(nombreMatch, marcaMatch, categoriaMatch));
             }
             // Filtro opcional por categoría
             if (categoria != null && !categoria.trim().isEmpty()) {
-                predicates.add(cb.equal(root.get("categoria"), categoria));
+                String categoriaPattern = "%" + categoria.toLowerCase().trim()+"%";
+                predicates.add(cb.like(cb.lower(root.get("categoria")), categoriaPattern));
             }
-            // Filtro por estado activo/inactivo (si no se envía, trae todos)
-            if (activo != null) {
-                predicates.add(cb.equal(root.get("activo"), activo));
+            // Filtro por estado activo/inactivo
+            if (estado != null && !estado.trim().isEmpty()) {
+                if (estado.trim().equalsIgnoreCase("activo")) {
+                    predicates.add(cb.equal(root.get("activo"), true));   
+                } else if (estado.trim().equalsIgnoreCase("inactivo")) {
+                    predicates.add(cb.equal(root.get("activo"), false));
+                }
+            // Si mandan algo distinto (como "todos"), simplemente no agregamos filtro y traerá ambos
             }
             return cb.and(predicates.toArray(new Predicate[0]));
         };
