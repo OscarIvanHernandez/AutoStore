9935e7ba39a8201a9c2d02576d3587f7ea341cf1
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-07-31 22:23:47 +0000

    Primeras anotaciones a los atributos de detalleVenta

--- backend/autostore-sys-web/src/main/java/com/padawan/spring/systems/autostore_sys_web/model/DetalleVenta.java
+++ backend/autostore-sys-web/src/main/java/com/padawan/spring/systems/autostore_sys_web/model/DetalleVenta.java
@@ -1,5 +1,40 @@
 package com.padawan.spring.systems.autostore_sys_web.model;
 
+import java.math.BigDecimal;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "detalleVentas")
+@EntityListeners(AuditingEntityListener.class)
+@Data
 public class DetalleVenta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "venta_id", nullable = false)
+    private Venta venta;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+    
+    private Integer cantidad;
+    private BigDecimal precioUnitario;
+    private BigDecimal subtotal;
 
 }
