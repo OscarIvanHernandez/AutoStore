2e883e3dad4b5230e16184e4dfba84d5573ab514
OscarIvanHernandez <39203406@uaz.edu.mx>
2026-08-04 19:23:21 +0000

    Servicio para crear y obetener ventas de hoy implementados

--- frontend/src/app/services/autostore.sales-service.ts
+++ frontend/src/app/services/autostore.sales-service.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { tap } from 'rxjs/operators';
+import { VentaRequest } from "./autostore.models";
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +11,23 @@ export class SaleService {
 
   private apiURL = 'http://localhost:8080/api/ventas';
 
+  constructor(private http: HttpClient){}
 
+  crearVenta(venta: VentaRequest): Observable<any> {
+    console.log('📡 Petición GET a:', this.apiURL);
+    return this.http.post<any>(this.apiURL, venta).pipe(
+      tap(response => {
+        console.log('📊 Respuesta recibida en AutoStore/   Venta-Service:', response);
+      })
+    );
+  }
+
+  obtenerVentasDeHoy(): Observable<any> {
+    console.log('📡 Petición GET a:', this.apiURL);
+    return this.http.get<any>(`${this.apiURL}/hoy`).pipe(
+      tap(response => {
+        console.log('📊 Respuesta recibida en AutoStore/   Venta-Service:', response);
+      })
+    );
+  }
 }
